bdf05c4a0a78d23e7a9699a2073f78c1a490078b
James Meyer <56805328+jamesmeyer1021@users.noreply.github.com>
2025-09-11 18:12:30 -0400

    Update sheets.py to use env-based credentials

--- app/sheets.py
+++ app/sheets.py
@@ -1,11 +1,26 @@
 # sheets.py
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import os
+from google.oauth2 import service_account
+
+creds_dict = {
+    "type": os.environ["GOOGLE_TYPE"],
+    "project_id": os.environ["GOOGLE_PROJECT_ID"],
+    "private_key_id": os.environ["GOOGLE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["GOOGLE_PRIVATE_KEY"].replace('\\n', '\n'),
+    "client_email": os.environ["GOOGLE_CLIENT_EMAIL"],
+    "client_id": os.environ["GOOGLE_CLIENT_ID"],
+    "auth_uri": os.environ["GOOGLE_AUTH_URI"],
+    "token_uri": os.environ["GOOGLE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["GOOGLE_AUTH_PROVIDER_CERT_URL"],
+    "client_x509_cert_url": os.environ["GOOGLE_CLIENT_CERT_URL"],
+    "universe_domain": os.environ["GOOGLE_UNIVERSE_DOMAIN"]
+}
 
 def get_sheet():
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+    creds = service_account.Credentials.from_service_account_info(creds_dict, scopes=scope)
     client = gspread.authorize(creds)
     return client.open("KiwisOrders")
 
